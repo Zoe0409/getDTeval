ebea289113a767deba52a2625d6d2afc072367f3
Zoe0409 <46760923+Zoe0409@users.noreply.github.com>
2019-12-27 18:48:17 -0500

    Set up the structure and basic document for this package

---
+++ R/getDTeval.R
@@ -0,0 +1,43 @@
+#' getDTeval
+#'
+#' @description getDTeval offers a method of translating coding statements into more efficient versions for improved runtime performance.  Using the get() and eval() functions allows for more programmatic coding designs that enable greater flexibility and more dynamic computations.  However, in data.table statements, get() and eval() reduce the efficiency of the method by performing work prior to data.table's optimized computations.  The getDTeval() function translates get() and eval() statements more efficiently.  This allows a user to both incorporate programmatic designs and while utilizing data.table's efficient processing routines.
+#'
+#' @param the.statement a character value expressing a coding statement, such as the data.table statement \code{dt[Age < 50, .(mean(Income)), by = "Region"]} or the dplyr statement dt %>% filter(Age < 50) %>% group_by(Region) %>% summarize(mean(Income)).  When the.statement includes variables that are called by reference to a naming constant using get() or eval(), then these statements are translated to  names of the variables.  So, for instance, with income.name = "Income", the statement \code{dt[, mean(get(income.name))]} would be translated to \code{dt[, mean(Income)]}.  In some cases this can lead to substantial improvements in speed, especially for large samples of data.  Meanwhile, the programmatic use of code facilitated by get() and eval() can be maintained.
+#' @param return.as a character value stating what should be returned.  When return.as = "result", the calculation is evaluated.  When return.as = "code", then the translated coding statement is provided.  When return.as = "all", then a list is returned that includes both the result and the code.
+#' @param eval.type a character value stating whether the coding statement should be evaluated in its current form (eval.type = "as.is") or have its called to get() and eval() translated (eval.type = "optimized", the default setting).
+#' @param envir The environment in which the calculation takes place, with the global environment .GlobalEnv set as the default.
+#' @param ... additional arguments to be passed.
+#'
+#' @source getDTeval::translate.fn.calls
+#' @export
+getDTeval <- function(the.statement, return.as = "result", eval.type = "optimized", envir = .GlobalEnv, ...){
+
+  if(!is.character(the.statement) & !is.expression(x = the.statement)){
+    return("Error:  the.statement must be a character or expression.")
+  }
+
+  the.statement <- as.character(the.statement)
+
+  if(eval.type != "as.is"){
+    pattern.get <- "get("
+    pattern.eval <- "eval("
+
+    num.get.calls <- length(grep(pattern = pattern.get, x = the.statement, fixed = TRUE))
+    num.eval.calls <- length(grep(pattern = pattern.eval, x = the.statement, fixed = TRUE))
+
+    if(num.get.calls > 0 | num.eval.calls > 0){
+      the.statement <- translate.fn.calls(the.statement = the.statement, function.name = pattern.get, envir = envir, ...)
+
+      the.statement <- translate.fn.calls(the.statement = the.statement, function.name = pattern.eval, envir = envir, ...)
+    }
+  }
+
+  if(return.as == "code"){
+    return(the.statement)
+  }
+  if(return.as == "all"){
+    return(list(result = eval(expr = parse(text = the.statement), envir = envir), code = the.statement))
+  }
+
+  return(eval(expr = parse(text = the.statement), envir = envir))
+}
