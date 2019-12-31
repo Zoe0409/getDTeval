51a412f2c1dae653e5e31a0418e79fe9a1c12dd2
dachosen1 <anderson.nelson1@gmail.com>
2019-12-31 02:13:19 -0500

    Format pacakge description

--- R/benchmark.getDTeval.R
+++ R/benchmark.getDTeval.R
@@ -1,11 +1,18 @@
 #' benchmark.getDTeval
 #'
-#' @description Performs a benchmarking experiment for data.table coding statements that use get() or eval() for programmatic designs.  The a) original statement is compared to b) passing the original statement through getDTeval and also to c) an optimized coding statement.  The results can demonstrate the overall improvement of using the coding translations offered by getDTeval().
+#' @description Performs a benchmarking experiment for data.table coding statements that
+#' use get() or eval() for programmatic designs.  The a) original statement is compared to b)
+#' passing the original statement through getDTeval and also to c) an optimized coding statement.
+#' The results can demonstrate the overall improvement of using the coding translations offered by getDTeval().
 #'
-#' @param the.statement a character value expressing a data.table calculation, such as \code{dt[Age < 50, .(mean(Income)), by = "Region"]}. When the.statement includes variables that are called by reference to a naming constant using get() or eval(), then these statements are translated into the names of the variables for substantial improvements in speed.
+#' @param the.statement a character value expressing a data.table calculation, such
+#' as \code{dt[Age < 50, .(mean(Income)), by = "Region"]}. When the.statement includes variables that are
+#'  called by reference to a naming constant using get() or eval(), then these statements are translated into
+#'   the names of the variables for substantial improvements in speed.
 #' @param times The number of iterations to run the benchmarking experiment.
 #' @param seed an integer value specifying the seed of the pseudorandom number generator.
-#' @param envir The environment in which the calculation takes place, with the global environment .GlobalEnv set as the default.
+#' @param envir The environment in which the calculation takes place, with the global environment .GlobalEnv
+#' set as the default.
 #' @param ... additional arguments to be passed.
 #'
 #' @source getDTeval::getDTeval
--- R/translate.fn.calls.R
+++ R/translate.fn.calls.R
@@ -2,9 +2,14 @@
 #'
 #' @description Internal function
 #'
-#' @param the.statement a character value expressing a data.table calculation, such as \code{dt[Age < 50, .(mean(Income)), by = "Region"]}. When the.statement includes variables that are called by reference to a naming constant using get() or eval(), then these statements are translated into the names of the variables for substantial improvements in speed.
-#' @param function.name translation basis either "get(" or "eval(" to perform the corresponding function. Set as "get(" as default.
-#' @param envir The environment in which the calculation takes place, with the global environment .GlobalEnv set as the default.
+#' @param the.statement a character value expressing a data.table calculation, such
+#' as \code{dt[Age < 50, .(mean(Income)), by = "Region"]}. When the.statement includes variables that are
+#' called by reference to a naming constant using get() or eval(), then these statements are translated into
+#' the names of the variables for substantial improvements in speed.
+#' @param function.name translation basis either "get(" or "eval(" to perform the corresponding function.
+#' Set as "get(" as default.
+#' @param envir The environment in which the calculation takes place, with the global environment .GlobalEnv
+#' set as the default.
 #' @param ... additional arguments to be passed.
 #'
 #' @import formulaic
